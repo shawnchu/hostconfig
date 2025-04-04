5eef52e652555db651bee7598298d081df3eab90
Shawn Chu <shawnchu@google.com>
2025-04-03 22:00:16 -0700

    fix
    Signed-off-by: Shawn Chu <shawnchu@google.com>

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 # update and export current IP address.
+#
+# Usage: crontab -e
+# # m h  dom mon dow   command
+# 5 22   *   *   *    ~/hostconfig/update.sh
 cd ~/hostconfig || exit
 published=$(cat host.conf)
 my_ip=$(curl -4 ifconfig.me)
